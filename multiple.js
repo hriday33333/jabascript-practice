f2c9b40b94e8556bd2147367f25c630a516e4436
hriday33333 <zakirhasanhriday@gmail.com>
2026-06-23 21:41:10 +0600

    2nd commit

---
+++ multiple.js
@@ -0,0 +1,9 @@
+const price = 50000
+const discount = 5
+
+if(price >=50000){
+  const discountPrice = price * discount / 100
+  console.log(discountPrice);
+  const finalPrice = price - discountPrice
+console.log(finalPrice)
+}
---
+++ nasted.js
@@ -0,0 +1,10 @@
+const mony = 50
+ if(mony > 500){
+  console.log('cinama plax  jabo')
+ }
+ else if(mony < 500){
+  console.log('coneplax a jabo na')
+ }
+else{
+  console.log('mony is exactly 500')
+}
